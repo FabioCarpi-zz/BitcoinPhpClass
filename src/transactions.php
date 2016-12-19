36a408cb6b93be8fae364724f05284f75f8d7ba8
Fabio <fabiocarpi@hotmail.com.br>
2016-12-19 10:42:18 -0200

    New functions and more
    - The version is now locked to 1;
    - 2 new functions to set or get the locktime;
    - The script for add input can be null;
    - Counting inputs and outputs;
    - The address for output add can be null;
    - Functions to get input and output;
    - The vout and sequence have to be in hex;
    - Check amount of in and out to build raw;

--- src/transactions.php
+++ src/transactions.php
@@ -1,22 +1,35 @@
 <?php
-// Version 1 from 2016-11-04
+// Version 1 from 2016-12-18
 require_once("keys.php");
 require_once("script.php");
 
 class Transaction extends Functions{
   private $TX = array(), $Raw;
 
-  /**
-   * 
-   * @param int $Version
-   * @param int $Lock
-   */
-  public function __construct($Version = 1, $Lock = 0){
+  public function __construct(){
     parent::__construct();
     $this->TX["hash"] = null;
     $this->TX["size"] = 0;
-    $this->TX["version"] = $Version;
-    $this->TX["locktime"] = $Lock;
+    $this->TX["version"] = 1;
+    $this->TX["locktime"] = 0;
+  }
+
+  /**
+   * 
+   * @param int $LockTime
+   */
+  public function LockSet($LockTime){
+    if(strlen($LockTime) <= 8){
+      $this->TX["locktime"] = $LockTime;
+    }
+  }
+
+  /**
+   * 
+   * @return int
+   */
+  public function LockGet(){
+    return $this->TX["locktime"];
   }
 
   /**
@@ -26,7 +39,13 @@ class Transaction extends Functions{
    * @param string $Script
    * @param int $Sequence
    */
-  public function InputAdd($PrevOut, $Vout, $Script, $Sequence = 0xffffffff){
+  public function InputAdd($PrevOut, $Vout, $Script = null, $Sequence = 0xffffffff){
+    if(empty($Script)){
+      $Script = null;
+    }
+    if(!ctype_xdigit($PrevOut) or !ctype_xdigit($Vout) or !ctype_xdigit($Sequence) or (!is_null($Script) and !ctype_xdigit($Script))){
+      return false;
+    }
     $this->Raw = null;
     $sc = new Script();
     $pointer = &$this->TX["vin"][];
@@ -38,12 +57,49 @@ class Transaction extends Functions{
   }
 
   /**
+   *
+   * @return int
+   */
+  public function InputCount(){
+    return count($this->TX["vin"]);
+  }
+
+  /**
+   *
+   * @param int $Index
+   * @return array
+   */
+  public function InputGet($Index){
+    return array(
+      "txid" => $this->TX["vin"][$Index]["txid"],
+      "vout" => $this->TX["vin"][$Index]["vout"],
+      "scriptsig" => $this->TX["vin"][$Index]["scriptSig"]["hex"],
+      "sequence" => $this->TX["vin"][$Index]["sequence"]
+    );
+  }
+
+  /**
    * 
    * @param int $Amount
    * @param string $Address
    * @param string $CustomScript
    */
-  public function OutputAdd($Amount, $Address, $CustomScript = null){
+  public function OutputAdd($Amount, $Address = null, $CustomScript = null){
+    if(empty($Address)){
+      $Address = null;
+    }
+    if(empty($CustomScript)){
+      $CustomScript = null;
+    }
+    if(is_null($Address) and !ctype_xdigit($CustomScript)){
+      return false;
+    }
+    if(is_null($CustomScript) and !ctype_alnum($Address)){
+      return false;
+    }
+    if(!ctype_digit($Amount)){
+      return false;
+    }
     $this->Raw = null;
     $pointer = &$this->TX["vout"][];
     $pointer["value"] = $Amount;
@@ -67,6 +123,29 @@ class Transaction extends Functions{
       $pointer["scriptPubKey"]["addresses"][] = null;
     }
   }
+
+  /**
+   *
+   * @return int
+   */
+  public function OutputCount(){
+    return count($this->TX["vout"]);
+  }
+
+  /**
+   *
+   * @param int $Index
+   * @return array
+   */
+  public function OutputGet($Index){
+    return array(
+      "address" => $this->TX["vout"][$Index]["scriptPubKey"]["addresses"][0],
+      "value" => $this->TX["vout"][$Index]["value"],
+      "scriptPubKeyhex" => $this->TX["vout"][$Index]["scriptPubKey"]["hex"],
+      "scriptPubKeyasm" => $this->TX["vout"][$Index]["scriptPubKey"]["asm"]
+    );
+  }
+
 
   /**
    * 
@@ -111,18 +190,22 @@ class Transaction extends Functions{
   private function RawBuild(){
     $return = reset(unpack("H*", pack("V*", $this->TX["version"])));
     $return .= reset(unpack("H*", pack("C*", count($this->TX["vin"]))));
-    foreach($this->TX["vin"] as $pt){
-      $return .= parent::SwapOrder($pt["txid"]);
-      $return .= reset(unpack("H*", pack("V*", $pt["vout"])));
-      $return .= reset(unpack("H*", pack("C*", strlen($pt["scriptSig"]["hex"]) / 2)));
-      $return .= $pt["scriptSig"]["hex"];
-      $return .= reset(unpack("H*", pack("V*", $pt["sequence"])));
+    if(count($this->TX["vin"]) > 0){
+      foreach($this->TX["vin"] as $pt){
+        $return .= parent::SwapOrder($pt["txid"]);
+        $return .= reset(unpack("H*", pack("h*", $pt["vout"])));
+        $return .= reset(unpack("H*", pack("C*", strlen($pt["scriptSig"]["hex"]) / 2)));
+        $return .= $pt["scriptSig"]["hex"];
+        $return .= reset(unpack("H*", pack("h*", $pt["sequence"])));
+      }
     }
     $return .= reset(unpack("H*", pack("C*", count($this->TX["vout"]))));
-    foreach($this->TX["vout"] as $pt){
-      $return .= reset(unpack("H*", pack("P*", $pt["value"])));
-      $return .= reset(unpack("H*", pack("C*", strlen($pt["scriptPubKey"]["hex"]) / 2)));
-      $return .= $pt["scriptPubKey"]["hex"];
+    if(count($this->TX["vout"]) > 0){
+      foreach($this->TX["vout"] as $pt){
+        $return .= reset(unpack("H*", pack("P*", $pt["value"])));
+        $return .= reset(unpack("H*", pack("C*", strlen($pt["scriptPubKey"]["hex"]) / 2)));
+        $return .= $pt["scriptPubKey"]["hex"];
+      }
     }
     $return .= reset(unpack("H*", pack("V*", $this->Lock)));
     $this->Raw = $return;
